60b5a2957e406e202a3d412b874ad066368b1c09
Saksham Dhuria <107601836+sakshamdhuria@users.noreply.github.com>
2025-12-01 03:54:51 -0600

    Update day1.2.cpp

--- 2025/day1.2.cpp
+++ 2025/day1.2.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-#define int long long
+#define int long long 
+#define PRINT_VECTOR(vec) do { \
+    for (size_t i = 0; i < vec.size(); ++i) { \
+        cout << vec[i] << (i + 1 < vec.size() ? " " : "\n"); \
+    } \
+} while(0)
 
 int32_t main() {
-    string filename = "adventOfCodeInput.txt";
+    string filename = "adventOfCodeInput.txt"; // Name of the input file
     ifstream inputFile(filename);
 
     string s;
-    int ans = 0, curr = 50;
-
-    while (getline(inputFile, s)) {
+    int ans=0, curr=50; 
+    while(getline(inputFile, s)){
         int num = stoll(s.substr(1));
-        while (num--) {
-            if (s[0] == 'R') curr++;
-            else curr--;
-
-            curr = (curr + 100) % 100; 
-            ans += (curr == 0);      
+        if(s[0]=='L'){
+            ans+=(num-curr)/100 + (num>=curr) - (curr==0);
+            curr-=num;
+        }else{
+            ans+=(num-(100-curr))/100 + (num>=100-curr);
+            curr+=num;
         }
-    }
+        curr=((curr%100)+100)%100;
 
+    }
+    
     cout << "ANS: " << ans << endl;
     return 0;
 }
